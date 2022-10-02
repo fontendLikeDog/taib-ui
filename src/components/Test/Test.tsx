fb98855f7f5e63a3ace4abf2f3f0b75ea30f2627
longstupay <761798834@qq.com>
2022-10-02 09:45:06 +0800

    tailwindcss init config

---
+++ src/components/Test/Test.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+// @ts-ignore
+import TestStyles from './Test.module.css';
+
+export interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
+    children: React.ReactNode;
+    primary?: boolean;
+    label?: string;
+}
+
+export const Test: React.FC<ButtonProps> = ({
+    children,
+    label,
+    ...props
+  }) => {
+   
+    const classNames = TestStyles;
+    console.log(classNames);
+    return (
+      <button className={classNames.header} {...props}>
+        {children || label}
+      </button>
+    );
+  };
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    './src/**/**/*.{js,ts,jsx,tsx,html,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,html,mdx}',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
